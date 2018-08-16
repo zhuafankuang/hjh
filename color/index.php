286f17a1f6494e394385f972a328b5b7609bf9d9
unknown <hanjh1@staff.ifeng.com>
2018-08-16 14:45:46 +0800

    first

---
+++ color/index.php
@@ -0,0 +1,18 @@
+
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>凤凰网-颜色选择器</title>
+</head>
+<link rel="stylesheet" type="text/css" href="spectrum.css">
+<script type="text/javascript" src="jquery.min.js"></script>
+<script type="text/javascript" src="spectrum.js"></script>
+<script type='text/javascript' src='docs.js'></script>
+
+<body>
+	<div style='text-align:center;'>
+    	<input id="full" />
+    </div>
+</body>
+</html>
